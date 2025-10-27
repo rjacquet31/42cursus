c78b794e63fa4f250eda04d228bf14f7ba7d843b
deluxeee31 <rafael.jacquet.31@gmail.com>
2025-10-27 11:48:21 +0100

    better visual

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelectorAll('.planet').forEach(planet => {
   planet.addEventListener('click', () => {
     const url = planet.dataset.url;
-    window.open(url, '_blank');
+    if (url) window.open(url, '_blank');
   });
 });
+
